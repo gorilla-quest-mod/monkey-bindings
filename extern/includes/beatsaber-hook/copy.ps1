4f4da6b2cde0bc347a27be59aa3f0a68d540b420
keganpowers34 <42286354+keganpowers34@users.noreply.github.com>
2022-07-07 17:37:07 -0600

    LMFAOOOOOOOOO

--- extern/includes/beatsaber-hook/copy.ps1
+++
@@ -1,4 +0,0 @@
-.\build.ps1
-
-& adb push ./build/libbeatsaber-hook_1_2_3.so /sdcard/Android/data/com.beatgames.beatsaber/files/libs/libbeatsaber-hook_1_2_3.so
-Copy-Item -Force build/libbeatsaber-hook_1_0_0.so ../libbeatsaber-hook_1_0_0.so
