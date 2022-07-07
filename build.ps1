9af0776fc65d72824fcecdfb13127f488f8662a2
keganpowers34 <42286354+keganpowers34@users.noreply.github.com>
2022-07-07 17:27:26 -0600

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,9 +1,43 @@
-Param(
-    [Parameter(Mandatory=$false)]
-    [Switch]$clean
+Param (
+[Parameter(Mandatory=$false, HelpMessage="The version the mod should be compiled with")][Alias("ver")][string]$Version,
+[Parameter(Mandatory=$false, HelpMessage="Switch to create a clean compilation")][Alias("rebuild")][Switch]$clean,
+[Parameter(Mandatory=$false, HelpMessage="To create a release build")][Alias("publish")][Switch]$release,
+[Parameter(Mandatory=$false, HelpMessage="To create a github actions build, assumes specific Environment variables are set")][Alias("github-build")][Switch]$actions,
+[Parameter(Mandatory=$false, HelpMessage="To create a debug build (Does not strip other libs)")][Alias("debug-build")][Switch]$debugbuild
 )
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+$QPMpackage = "./qpm.json"
+$qpmjson = Get-Content $QPMpackage -Raw | ConvertFrom-Json
+$ModID = $qpmjson.info.id
+if (-not $Version) {
+    $VERSION = $qpmjson.info.version
+} else {
+    $VERSION = $Version
+}
+if ($release -ne $true -and -not $VERSION.Contains('-Dev')) {
+    $VERSION += "-Dev"
+}
 
-# if user specified clean, remove all build files
+if ($env:version -eq "") {
+    & qpm-rust package edit --version $VERSION
+}
+
+if ((Test-Path "./extern/includes/beatsaber-hook/src/inline-hook/And64InlineHook.cpp", "./extern/includes/beatsaber-hook/src/inline-hook/inlineHook.c", "./extern/includes/beatsaber-hook/src/inline-hook/relocate.c") -contains $false) {
+    Write-Host "Critical: Missing inline-hook"
+    if (!(Test-Path "./extern/includes/beatsaber-hook/src/inline-hook/And64InlineHook.cpp")) {
+        Write-Host "./extern/includes/beatsaber-hook/src/inline-hook/And64InlineHook.cpp"
+    }
+    if (!(Test-Path "./extern/includes/beatsaber-hook/src/inline-hook/inlineHook.c")) {
+        Write-Host "./extern/includes/beatsaber-hook/src/inline-hook/inlineHook.c"
+    }
+        if (!(Test-Path "./extern/includes/beatsaber-hook/inline-hook/src/relocate.c")) {
+        Write-Host "./extern/includes/beatsaber-hook/src/inline-hook/relocate.c"
+    }
+    Write-Host "Task Failed, see output above"
+    exit 1;
+}
+echo "Building mod $ModID version $VERSION"
+
 if ($clean.IsPresent)
 {
     if (Test-Path -Path "build")
@@ -12,14 +46,19 @@ if ($clean.IsPresent)
     }
 }
 
-$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
-
 if (($clean.IsPresent) -or (-not (Test-Path -Path "build")))
 {
     $out = new-item -Path build -ItemType Directory
-} 
+}
 
 cd build
-& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="Release" ../
-& cmake --build .
+if ($debugbuild -eq $true) {
+& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DDEBUG=true ../
+} else {
+& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" ../
+}
+& cmake --build . -j 6
+$ExitCode = $LastExitCode
 cd ..
+exit $ExitCode
+echo Done
